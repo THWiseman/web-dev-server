42e25807a86b5149a00902f54bbb914b04ccf487
THWiseman <77803506+twiseman1992@users.noreply.github.com>
2022-04-15 11:40:28 -0400

    a9 stuff

--- controllers/tuits.js
+++ controllers/tuits.js
@@ -5,7 +5,7 @@ export default [
         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
         "likes": 345,
         "dislikes" : 12,
-        "stats": {"comments" : 1234, "retuits" : "5678", "likes" : "6968"}
+        "stats": {"comments" : 1234, "retuits" : "5678", "likes" : "6968", "dislikes":"12"}
 
 
     },
@@ -15,7 +15,7 @@ export default [
         "tuit": "Ingenuity helicopter takes flight.",
         "likes": 31775,
         "dislikes" : 100,
-        "stats": {"comments" : "2564", "retuits" : "444", "likes" : "4511"}
+        "stats": {"comments" : "2564", "retuits" : "444", "likes" : "4511", "dislikes":"100"}
     },
     {
         "_id": "345",
@@ -23,6 +23,6 @@ export default [
         "tuit": "Jquery likes java more than javascript.",
         "likes": 50,
         "dislikes" : 3,
-        "stats": {"comments" : "2564", "retuits" : "444", "likes" : "4511"}
+        "stats": {"comments" : "2564", "retuits" : "444", "likes" : "4511", "dislikes":"3"}
     }
 ];
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
-import tuitController from "./controllers/tuits-controller.js"
+import tuitController from "./controllers/tuits-controller.js";
+import mongoose from 'mongoose'
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(express.json());
